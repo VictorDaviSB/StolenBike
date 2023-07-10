da8d014dac7ef95d3beb4a131dd76c2937d93c92
VictorDaviSB <107366650+VictorDaviSB@users.noreply.github.com>
2023-07-09 21:37:13 -0300

    Update Código Controlador.ino

--- Código Controlador.ino
+++ Código Controlador.ino
@@ -22,14 +22,15 @@
 //-----------------------------------------------------------------------------------
 
 //-----------------------------------------------------------------------------------
+//definição do host e da key para acesso ao banco de dados
 #define FIREBASE_HOST "https://anti-furto-de4a9-default-rtdb.firebaseio.com/"
 #define FIREBASE_AUTH "9DVtXOpVu9LMrwUR02IJewsL5i9Hm9PEmGuwrpNi"
+// Definição das credenciais de acesso ao wifi (Deve ser alterada caso seja desejado conectar em outra rede)
 #define WIFI_SSID "Mono-net"
 #define WIFI_PASSWORD "Bike_CE289"
 //-----------------------------------------------------------------------------------
-//#define WIFI_SSID "Goiabeira-2G"
-//#define WIFI_PASSWORD "monstrodosabao"
-Servo myservo;
+
+Servo myservo; // Definição do servo
 int DistanciaTotal;
 //-----------------------------------------------------------------------------------
 //Define FirebaseESP8266 data object
@@ -42,14 +43,12 @@ FirebaseJson json;
 //GPS Module RX pin to NodeMCU D1
 //GPS Module TX pin to NodeMCU D2
 const int RXPin = 4, TXPin = 5;
-SoftwareSerial neo6m(RXPin, TXPin);
+SoftwareSerial neo6m(RXPin, TXPin); //pinos do GPS
 TinyGPSPlus gps;
 //-----------------------------------------------------------------------------------
-int distancia;
-int rotacao = 100;
 bool furtada = false;
 //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-void setup()
+void setup() //setup do projeto (inicialização das portas seriais, da conexão com o wifi e com o Firebase)
 {
   myservo.attach(D4);
   Serial.begin(9600);
@@ -68,16 +67,16 @@ void setup()
 
 
 //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-void loop() {
-  distancia = random(100);
+void loop() { // loop do código
+ 
   smartdelay_gps(1000);
-  rotacao = -rotacao;
-  if(!furtada){
+ 
+  if(!furtada){ // se a bicicleta não estiver furtada, voltar o servo para a posição de relaxamento do barbante
     myservo.write(0);
   }
   
   
-  if(gps.location.isValid()) 
+  if(gps.location.isValid()) // quando as informações do gps forem válidas (satélites forem identificados)
   {
     float latitude = gps.location.lat();
     float longitude = gps.location.lng();
@@ -91,16 +90,16 @@ void loop() {
     //-------------------------------------------------------------
     
     //-------------------------------------------------------------
-    if(Firebase.setFloat(firebaseData, "/GPS/f_latitude", latitude))
+    if(Firebase.setFloat(firebaseData, "/GPS/f_latitude", latitude)) //envia os dados de latitude
       {print_ok();}
     else
       {print_fail();}
     //-------------------------------------------------------------
-    if(Firebase.setFloat(firebaseData, "/GPS/f_longitude", longitude))
+    if(Firebase.setFloat(firebaseData, "/GPS/f_longitude", longitude)) //envia os dados de longitude
       {print_ok();}
     else
       {print_fail();}
-     if(Firebase.setFloat(firebaseData, "/GPS/velocidade", velocidade))
+     if(Firebase.setFloat(firebaseData, "/GPS/velocidade", velocidade)) // envia os dados de velocidade
       {print_ok();}
     else
       {print_fail();}
@@ -110,18 +109,18 @@ void loop() {
   {
     Serial.println("No valid GPS data found.");
   }
-  if(Firebase.getInt(firebaseData, "/GPS/distanciaTotal")){
-
+  if(Firebase.getInt(firebaseData, "/GPS/distanciaTotal")){ //Recebe os dados da distancia do Banco de Dados
+   
   
       if(firebaseData.dataTypeEnum() == fb_esp_rtdb_data_type_integer){
-        Serial.println(firebaseData.to<int>());
+        Serial.println(firebaseData.to<int>()); 
 
         DistanciaTotal = firebaseData.to<int>();
-        if(Firebase.getFloat(firebaseData,"GPS/velocidade")){
+        if(Firebase.getFloat(firebaseData,"GPS/velocidade")){ // Recebe os dados de velocidade
 
-        if(DistanciaTotal > 20 && firebaseData.to<float>() > 3){
-          myservo.write(180);
-          furtada = true;
+        if(DistanciaTotal > 30 && firebaseData.to<float>() > 4){ // caso a distância for maior que 30 metros e a velocidade maior que 4km/h
+          myservo.write(180); //fechar o servo
+          furtada = true; // bike furtada!!!
         }
         else{
           furtada = false;
@@ -156,7 +155,7 @@ static void smartdelay_gps(unsigned long ms)
 
 
 //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-void wifiConnect()
+void wifiConnect() // Conexão com o Wifi
 {
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
